1f53dbef0ccdb43e4c808710422025d281aae051
marinagutu <m.gutu@topmotive.eu>
2023-06-23 10:19:36 +0300

    Loading component

---
+++ components/common/Loading.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { View, ActivityIndicator, Text, StyleSheet } from "react-native";
+import { COLORS } from "../../constants";
+
+const Loading = () => {
+  return (
+    <View style={style.container}>
+      <ActivityIndicator size={"large"} color={COLORS.black} />
+      <Text style={style.title}>Loading...</Text>
+      <Text style={style.subtitle}>Please wait while data is loading.</Text>
+    </View>
+  );
+};
+
+const style = StyleSheet.create({
+  container: {
+    justifyContent: "center",
+    alignContent: "center",
+    alignItems: "center",
+    marginTop: -50,
+  },
+  title: {
+    fontSize: 26,
+    fontWeight: "bold",
+    marginTop: 10,
+  },
+  subtitle: {
+    color: COLORS.grayLight,
+    fontSize: 16,
+    marginTop: 5,
+  },
+});
+
+export default Loading;
